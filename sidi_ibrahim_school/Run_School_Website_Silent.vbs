2e3fb7bfc8a7bea94f93ee04fe5c6eaf79ba27ef
lakrimi99-web <lakrimi99@gmail.com>
2026-08-10 23:16:26 +0100

    Delete sidi_ibrahim_school directory

--- sidi_ibrahim_school/Run_School_Website_Silent.vbs
+++
@@ -1,2 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run chr(34) & "C:\Users\mlk\.gemini\antigravity\scratch\sidi_ibrahim_school\Run_School_Website.bat" & chr(34), 1, False
--- sidi_ibrahim_school/admin/includes/admin-footer.php
+++
@@ -1,7 +0,0 @@
-        </div> <!-- End admin-content -->
-    </div> <!-- End admin-main -->
-</div> <!-- End admin-layout -->
-
-<script src="../assets/js/main.js"></script>
-</body>
-</html>
